1d0d1448f7d8d9c45938739d0b6552abac1b9eb8
Daniel Malone <danieljmalone@gmail.com>
2017-05-04 21:07:26 -0500

    initial commit

---
+++ src/com/finepointstudios/Main.java
@@ -0,0 +1,53 @@
+package com.finepointstudios;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    // Constructor
+    Person(String firstName, String lastName, int identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    // Print person data
+    public void printPerson() {
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + "\nID: " + idNumber);
+    }
+
+}
+
+class Student extends Person {
+    private int[] testScores;
+
+    /*
+    *   Class Constructor
+    *
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    Student(String firstName, String lastName, int id, int[] scores) {
+        super(firstName, lastName, id);
+    }
+
+    /*
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    public String calculate() {
+        return "asdf";
+    }
+}
